420fbcee3e48b66ae574da855b5f1f983d27bd16
PatrickTaggart15 <patrickdtaggart15@gmail.com>
2025-01-24 01:34:05 -0700

    ChessMove is completed

--- shared/src/main/java/chess/moveCalculators/KingMoveCalculator.java
+++ shared/src/main/java/chess/moveCalculators/KingMoveCalculator.java
@@ -1,4 +1,17 @@
 package chess.moveCalculators;
 
-public class KingMoveCalculator {
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class KingMoveCalculator implements MoveCalculator {
+
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition currPosition) {
+        int[][] relativeMoves = {{-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}};
+        return MoveCalculator.generateStaticMoves(currPosition, relativeMoves, board);
+
+    }
+
 }
